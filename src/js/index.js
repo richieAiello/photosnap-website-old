b396559f2d094acd5dd9a9bee5ef7990b16bc10d
Richard Aiello <richaiello17@gmail.com>
2022-05-08 11:30:35 -0400

    Made hamburger button interactive.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,27 @@
 import { insertIcons } from "./insertSvgs";
 
+// Variables for nav
+const navOpen = document.querySelector('.btn--nav-open');
+const navClose = document.querySelector('.btn--nav-close');
+const navMenu = document.querySelector('.nav__menu');
+
+// Footer icon container
 export const iconContainer = document.querySelector('.footer__icons');
 
+// Inserts inline svgs into html to clear up html files and for styling.
 insertIcons();
+
+/********************************************
+  EVENT LISTENERS
+********************************************/
+navOpen.addEventListener('click', e => {
+  navMenu.style.display = "block";
+  navOpen.classList.toggle('hidden');
+  navClose.classList.toggle('hidden');
+});
+
+navClose.addEventListener('click', e => {
+  navMenu.style.display = "none";
+  navOpen.classList.toggle('hidden');
+  navClose.classList.toggle('hidden');
+});
